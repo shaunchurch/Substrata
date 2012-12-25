9c2f826374061862688eb987fec67644be22e35c
Shaun Church <shaunchurch@gmail.com>
2012-12-25 19:33:56 +0000

    Initial Commit

---
+++ js/main-ck.js
@@ -0,0 +1,1 @@
+function Substrata(){var e=this;this.init=function(){$(window).resize(function(){e.setupWindow()});this.setupWindow()},this.setupWindow=function(){}};
---
+++ js/main.js
@@ -0,0 +1,16 @@
+function Substrata() {
+    var self = this;
+
+    this.init = function() {
+
+        // resize event
+        $(window).resize(function() {
+                self.setupWindow();
+        });
+
+        this.setupWindow();
+    },
+    this.setupWindow = function() {
+          
+    };
+}
